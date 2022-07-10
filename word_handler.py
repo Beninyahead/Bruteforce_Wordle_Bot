39d02d9f2085017007c68eee6f3bfe41260165b4
Ben <ben3670@live.com.au>
2022-07-10 11:42:44 +1000

    refactored to accept word results as dict

--- word_handler.py
+++ word_handler.py
@@ -20,16 +20,16 @@ class WordHandler:
             filepath (str): File path to to a txt file of line seperated 5 letter words.
         """
         self.file_path = filepath
-        self.known_letters: dict = {}
-        self.present_letters: list = []
-        self.absent_letters: list = []
-        self.available_words: list = []
+        self.known_letters: dict[str, int] = {}
+        self.present_letters: dict[str, list[int]] = {}
+        self.absent_letters: list[str] = []
+        self.available_words: list[str] = []
         self.count = 0
         self.first_word_index: int = None 
 
         self.__read_words()
 
-    def __read_words(self):
+    def __read_words(self) -> None:
         """Read file to list"""
         logger.debug(f"reading data from {self.file_path}")
         with open(self.file_path, mode='r', encoding='utf-8') as file:
@@ -37,7 +37,7 @@ class WordHandler:
             self.available_words = [word.strip().lower() for word in data] 
 
 
-    def __remove_word(self, word:str):
+    def __remove_word(self, word:str) -> None:
         """Remove Word from self.available_Words
         Args:
             word (str): word to remove
@@ -48,7 +48,7 @@ class WordHandler:
             # raised if a other method has already removed the word.
             pass
 
-    def __remove_words_containing_unavailable_letters(self, word:str):
+    def __remove_words_containing_unavailable_letters(self, word:str) -> None:
         """Remove Words that contain letters that are not available
 
         Args:
@@ -58,7 +58,7 @@ class WordHandler:
             if letter.lower() in word.lower():
                 self.__remove_word(word)
 
-    def __remove_words_without_known_values(self, word:str):
+    def __remove_words_without_known_values(self, word:str) -> None:
         """Remove Words in list that do not contain a letter at the known index.
         Args:
             word (str): word to check
@@ -67,24 +67,63 @@ class WordHandler:
             if letter.lower() != word[index].lower():
                     self.__remove_word(word)
 
-    def __remove_words_without_present_values(self, word:str):
+    def __remove_words_without_present_values(self, word:str) -> None:
         """Remove Words in list that do not contain a letter at the known index.
         Args:
             word (str): word to check
         """
-        for letter in self.present_letters:
+        for letter in self.present_letters.keys():
             if letter not in word:
                 self.__remove_word(word)
+                break
 
-    def filter_word_list(self):
+    def __remove_words_with_present_letter_at_unaviable_index(self, word:str) -> None:
+        """Remove the word if it has a present letter at a already tried index.
+
+        Args:
+            word (str): word to check
+        """
+        for letter, index_list in self.present_letters.items():
+            for index in index_list:
+                if word[index] == letter:
+                    self.__remove_word(word)
+                    break
+
+    def filter_word_list(self) -> None:
         """Brute force Iterate through words, cleansing list"""
         logger.info("Filtering word list")
         for word in self.available_words[:]:
             self.__remove_words_containing_unavailable_letters(word)
             self.__remove_words_without_known_values(word)
             self.__remove_words_without_present_values(word)
+            self.__remove_words_with_present_letter_at_unaviable_index(word)
 
-    def guess_a_word(self):
+    def update_indexes(self, indexed_results:dict[int, tuple[str, str]]) -> None:
+        """Update the Known, absent, and unavailable attributes.
+
+        Args:
+            indexed_results (dict[int, tuple[str, str]]): Key value indexed results for word sente eg: `{0: ('l', 'absent'), 1: ('o', 'present')}`
+        """
+        corrrect_present_letters = [results[0] for _, results in indexed_results.items() if results[1] in ['correct', 'present']]
+        for index, results in indexed_results.items():
+            letter = results[0]
+            state = results[1]
+            # for duplicate letters, if absent letter is also marked as present or correct skip it.
+            if state == 'absent' and letter not in corrrect_present_letters:
+                self.absent_letters.append(letter)
+            elif state == 'correct':
+                self.known_letters[letter] = index 
+            elif state == 'present':
+                current_present_indexes = self.present_letters.get(letter, [])
+                current_present_indexes.append(index)
+                self.present_letters[letter] = current_present_indexes 
+        
+        #  Log updated data
+        logger.info(f'Correct letters: {self.known_letters}')
+        logger.info(f'Present letters: {self.present_letters}')
+        logger.info(f'Absent letters: {self.absent_letters}')
+
+    def guess_a_word(self) -> str:
         """Selects a random word from wordlists based on self.count
         if count is between 1 and 2: Use Constant, optimized starting words
         else: Use available word list.
@@ -114,7 +153,7 @@ class WordHandler:
                 return guess
                 
     @property
-    def closet_word(self):
+    def closet_word(self) -> str:
         """Build word string from known indexes"""
         word = ['?','?','?','?','?']
         for letter, index in self.known_letters.items():
